49f19d8886add4d122a2fe187d774b5dee84ba7a
Paulo Mesquita <paulo@mesquita.dev>
2021-07-02 10:57:35 -0300

    Delete build directory

--- build/loaders/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.loaders = void 0;
-var express_1 = require("./express");
-var loaders = function (app) {
-    express_1.expressLoader(app);
-};
-exports.loaders = loaders;

8b3ed3f4bdf3f795f601b7723b5796bc5cc16781
PauloMesquita <paulo@mesquita.dev>
2021-07-04 18:08:51 -0300

    create db connection using typeorm

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express';
 import { expressLoader } from './express';
+import { databaseLoader } from './database';
 
-export const loaders = (app: Express): void => {
+export const loaders = async (app: Express): Promise<void> => {
+  await databaseLoader();
   expressLoader(app);
 };
